2d66339d3099d80622a3c9069ed318f8e326da2d
tazz <tazz@tazz.cloud>
2025-12-15 11:47:33 -0500

    feat(init.zsh): :sparkles: add autoenv code

--- init.zsh
+++ init.zsh
@@ -1,5 +1,23 @@
 #!/usr/bin/env zsh
 PLUGIN_DIR="${0:a:h}"
 
-#TODO: implement
-echo "Hello World"
+__zsh_on_chpwd() {
+  if [[ "$PWD" == "$HOME" ]]; then
+    emulate -L zsh
+    eza -la
+  fi
+
+  local ZSH_DOTENV_FILE=".env"
+  if [[ -f "$ZSH_DOTENV_FILE" ]]; then
+    zsh -fn "$ZSH_DOTENV_FILE" || {
+      echo "dotenv: error when sourcing '$ZSH_DDTENV_FILE' file" >&2
+      return 1
+    }
+
+    setopt localoptions allexport
+    source "$ZSH_DOTENV_FILE"
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd __zsh_on_chpwd
